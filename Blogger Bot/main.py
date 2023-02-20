71fac10a7037af64e7bb5ab717fd5dfbae94a61e
FaisalKhan19 <102422412+FaisalKhan19@users.noreply.github.com>
2023-02-20 20:20:18 +0530

    Update main.py

--- Blogger Bot/main.py
+++ Blogger Bot/main.py
@@ -6,8 +6,8 @@ user = Scraper()
 user.login()
 
 prompts = user.scrape(20)
-
-ans = OpenAI.get_res(prompts,"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")
+api_key = "Your api key for open ai(visit-https://platform.openai.com/account/api-keys)"
+ans = OpenAI.get_res(prompts,api_key)
 
 for i, j in ans, prompts:
     print("Prompt is: ", j)
